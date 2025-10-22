3338e0fdb935463ec689349734659caa2a00c7b6
Kenneth McKrola <kennethmckrola@Kenneths-MacBook-Pro-2.local>
2025-10-22 17:14:31 -0600

    fixed origin allowed

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000','http://127.0.0.1:3000'], // ← explicit, not '*'
+    'allowed_origins' => ['*'],
     // or use allowed_origin_patterns if you prefer wildcards
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
